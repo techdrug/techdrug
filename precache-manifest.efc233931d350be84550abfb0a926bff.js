1eb997cf89b2039234fc2b241360c57c983ec634
Mohd <mohd@infino.co>
2020-08-31 16:30:02 +0530

    Updates

---
+++ precache-manifest.efc233931d350be84550abfb0a926bff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c912c5a0910ae158a2764658a1e8c8a5",
+    "url": "/techdrug/index.html"
+  },
+  {
+    "revision": "5869d5c08d10bd35ab9c",
+    "url": "/techdrug/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "303670ea9db1631d78b7",
+    "url": "/techdrug/static/js/2.20fea639.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/techdrug/static/js/2.20fea639.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5869d5c08d10bd35ab9c",
+    "url": "/techdrug/static/js/main.a776506a.chunk.js"
+  },
+  {
+    "revision": "cc0c8a192a3092cce9ed",
+    "url": "/techdrug/static/js/runtime-main.9071646a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/techdrug/static/media/logo.5d5d9eef.svg"
+  }
+]);
